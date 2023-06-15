6f0074d80534663fe1aa9f2b5f3e1d03e7fd6467
test <test@test.com>
2023-06-15 18:41:22 +0900

    Spring 2강 예제들

---
+++ src/main/java/com/example/config/DatabaseConfig.java
@@ -0,0 +1,28 @@
+package com.example.config;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+@Configuration		// 설정 관련된 Spring bean
+@MapperScan(basePackages = "com.example.*")  // mapper scan 추가!!!
+// DB Repository 찾기 위한 어노테이션
+public class DatabaseConfig {
+
+	@Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+
+        Resource[] res = new PathMatchingResourcePatternResolver().getResources("classpath:mappers/*Mapper.xml");
+        sessionFactory.setMapperLocations(res);
+
+        return sessionFactory.getObject();
+    }
+}
--- src/main/java/com/example/lesson02/bo/UsedGoodsBO.java
+++ src/main/java/com/example/lesson02/bo/UsedGoodsBO.java
@@ -2,18 +2,24 @@ package com.example.lesson02.bo;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.lesson02.dao.UsedGoodsMapper;
 import com.example.lesson02.domain.UsedGoods;
 
 @Service	// Spring Bean
 public class UsedGoodsBO {
 	
+	// 필드
+	@Autowired		// Dependency Inject (DI) -> 의존성 주입 => Spring bean
+	private UsedGoodsMapper usedGoodsMapper;  // = null 아직 아무것도 선언하지 않았으므로
+	
 	// input: X		컨트롤러 한테 주는게 없음
 	
 	// output: List<UsedGoods>		컨트롤러 한테
 	public List<UsedGoods> getUsedGoodsList() {
-		List<UsedGoods> usedGoodsList = ;
+		List<UsedGoods> usedGoodsList = usedGoodsMapper.selectUsedGoodsList();
 		return usedGoodsList;
 	}
 	
